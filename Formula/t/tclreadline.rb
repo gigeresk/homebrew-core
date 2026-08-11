b7dd62ec0c88d0c741fbe583c67686cfa863e578
Fred Tombs <fred@silimate.com>
2026-08-11 21:55:14 +0000

    tclreadline: use 2.4.0 instead of 2.4.1
    2.4.1 is effectively a docs bump and forgot to update AC_INIT, which
    forced a packaging patch. 2.4.0 is the last clean tagged release.

--- Formula/t/tclreadline.rb
+++ Formula/t/tclreadline.rb
@@ -1,8 +1,8 @@
 class Tclreadline < Formula
   desc "GNU Readline for interactive Tcl shells"
   homepage "https://github.com/flightaware/tclreadline"
-  url "https://github.com/flightaware/tclreadline/archive/refs/tags/v2.4.1.tar.gz"
-  sha256 "d14b1568b6db8cd51659e3cc476a1f45da2020434ebb90b4b0defbc424f05907"
+  url "https://github.com/flightaware/tclreadline/archive/refs/tags/v2.4.0.tar.gz"
+  sha256 "306f9a72ef3ad9ff7a8549f1c616edef5365b98bce7e62fdde6e595355c0faee"
   license "BSD-3-Clause"
 
   livecheck do
@@ -15,13 +15,6 @@ class Tclreadline < Formula
   depends_on "libtool" => :build
   depends_on "readline"
   depends_on "tcl-tk@8"
-
-  # Upstream forgot to bump AC_INIT for the v2.4.1 tag.
-  # https://github.com/flightaware/tclreadline/pull/62
-  patch do
-    url "https://github.com/flightaware/tclreadline/commit/0d793fc3ac647af977400b5673121bffcd57f6e3.patch?full_index=1"
-    sha256 "1b65a1df3cd81f12b4da30952bac5c65576675ac3d2ca4a2cb9da9fd56c6f83e"
-  end
 
   def install
     tcl = Formula["tcl-tk@8"]
